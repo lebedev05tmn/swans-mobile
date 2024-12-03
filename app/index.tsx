75100ba2983ef10140ad25a943045296d53618bc
k.lebedev <k.lebedev@teraplan.ru>
2024-12-03 20:05:56 +0300

    gluestack add

--- app/index.tsx
+++ app/index.tsx
@@ -5,12 +5,11 @@ import {
 } from '@expo-google-fonts/montserrat-alternates';
 import LoadingPage from '../pages/LoadingPage';
 
-//30 строчка рендер шрифта, вместо текста склетон лоудер из глюстака
-
 export default function HomeScreen() {
     const [loaded] = useFonts({
         MontserratAlternates_700Bold,
     });
+
     if (!loaded) {
         return null;
     }
