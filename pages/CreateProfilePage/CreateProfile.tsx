937223dec5d8a379e7e24a75f263f6b905ff4f5b
AndDorosh <andrey.dorosh@list.ru>
2024-12-13 19:14:36 +0300

    fix

--- pages/CreateProfilePage/CreateProfile.tsx
+++ pages/CreateProfilePage/CreateProfile.tsx
@@ -1,24 +1,29 @@
-import { View, StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from 'expo-linear-gradient';
 
-
 const CreateProfilePage = () => {
+    
+
     return (
-        <SafeAreaView>
-            <LinearGradient
-                        colors={['#B18FCF', '#87CEEB']}
-                        style={styles.gradient}
-                        start={{ x: 0.1, y: 0.1 }}
-                        end={{ x: 0.9, y: 0.7 }}
-            >
-                
-            </LinearGradient>
-        </SafeAreaView>
+        <LinearGradient
+                    colors={['#B18FCF', '#87CEEB']}
+                    style={styles.gradient}
+                    start={{ x: 0.1, y: 0.1 }}
+                    end={{ x: 0.9, y: 0.7 }}
+        >
+            <SafeAreaView style={styles.container}>
+                    
+                    
+            </SafeAreaView>
+        </LinearGradient>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     gradient: {
         flex: 1,
     },
