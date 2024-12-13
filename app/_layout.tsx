b86f2bc6a29377825c0b81a64b6d53c6b8adf365
AndDorosh <andrey.dorosh@list.ru>
2024-12-13 19:02:02 +0300

    screenOption settings

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,12 +3,18 @@ import { Stack } from 'expo-router';
 import '@/global.css';
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 
+
 export default function RootLayout() {
     return (
         <GluestackUIProvider mode="light">
             <Stack
                 screenOptions={{
                     headerShown: false,
+                    navigationBarTranslucent: true,
+                    navigationBarColor: 'transparent',
+                    statusBarTranslucent: true,
+                    statusBarBackgroundColor: 'transparent',
+                    statusBarStyle:  'dark',
                 }}
             ></Stack>
         </GluestackUIProvider>
