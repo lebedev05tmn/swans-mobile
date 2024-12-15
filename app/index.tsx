39435b909083a9705c5ba78ce1363e4cbfee8b40
AndDorosh <andrey.dorosh@list.ru>
2024-12-15 20:15:18 +0300

    добавлена поддержка шрифтов: semiBold, Medium, Regular

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import {
     useFonts,
     MontserratAlternates_700Bold,
+    MontserratAlternates_600SemiBold,
+    MontserratAlternates_500Medium,
+    MontserratAlternates_400Regular,
 } from '@expo-google-fonts/montserrat-alternates';
 import LoadingPage from '../pages/LoadingPage';
 
 export default function HomeScreen() {
     const [loaded] = useFonts({
         MontserratAlternates_700Bold,
+        MontserratAlternates_600SemiBold,
+        MontserratAlternates_500Medium,
+        MontserratAlternates_400Regular,
     });
 
     if (!loaded) {
