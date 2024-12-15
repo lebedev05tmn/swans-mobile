1aadef6489b13f9f775a627749e7ff33b52d1f19
AndDorosh <andrey.dorosh@list.ru>
2024-12-15 21:03:18 +0300

    feat: добавлены и настроены reanimated и svg

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 
 module.exports = (() => {
     const config = getDefaultConfig(__dirname);
@@ -17,5 +18,8 @@ module.exports = (() => {
         sourceExts: [...resolver.sourceExts, 'svg'],
     };
 
-    return withNativeWind(config, { input: './global.css' });
+    const nativeWindConfig = withNativeWind(config, { input: './global.css' });
+
+  
+    return wrapWithReanimatedMetroConfig(nativeWindConfig);
 })();
