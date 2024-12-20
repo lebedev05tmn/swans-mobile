995e577cc33bb640d7cdeee0e2d2e6f058dba89c
AndDorosh <andrey.dorosh@list.ru>
2024-12-20 18:02:47 +0300

    Ничего важного

--- pages/CreateProfilePage/CreateProfile.tsx
+++ pages/CreateProfilePage/CreateProfile.tsx
@@ -4,14 +4,10 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from 'expo-linear-gradient';
 import ArrowLeftIcon from "@/assets/svg/ArrowLeftIcon";
 import ProgressWave from "@/assets/svg/progress.svg";
-import pages from "./pages";
-import useStore from './store';
 
 
 const CreateProfilePage = () => {
-    const previousPage = useStore((state) => state.previousPage);
-    const currentIndex = useStore((state) => state.currentIndex);
-    const CurrentComponent = pages[currentIndex];
+    
 
     return (
         <LinearGradient
@@ -23,14 +19,12 @@ const CreateProfilePage = () => {
             <SafeAreaView style={styles.container}>
                 <View style={styles.header}>
                     <Pressable
-                        onPress={previousPage}
                         style={styles.arrowLeftIcon}
                     >
                         <ArrowLeftIcon />
                     </Pressable>
                     <ProgressWave width={'64%'} height={'auto'}/>
                 </View>
-                <CurrentComponent />
             </SafeAreaView>
         </LinearGradient>
     );
@@ -39,6 +33,7 @@ const CreateProfilePage = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingHorizontal: '7%',
     },
     gradient: {
         flex: 1,
@@ -48,7 +43,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         height: 30,
-        marginHorizontal: '7%',
         marginVertical: 28,
     },
     arrowLeftIcon: {
--- pages/CreateProfilePage/pages.tsx
+++ pages/CreateProfilePage/pages.tsx
@@ -1,76 +1,58 @@
-import CreateProfileContent from "@/components/CreateProfileContent/CreateProfileContent";
-import CountinueButton from "@/components/CountinueButton/CountinueButton";
-import NameInput from "@/components/NameInput/NameInput";
-import PlaceInput from "@/components/PlaceInput/PlaceInput";
-import DateInput from "@/components/DateInput/DateInput";
 import AboutTextInput from "@/components/AboutTextInput/AboutTextInput";
 import ButtonChoice from "@/components/ButtonChoice/ButtonChoice";
-import useStore from "./store";
+import DateInput from "@/components/DateInput/DateInput";
+import NameInput from "@/components/NameInput/NameInput";
+import PlaceInput from "@/components/PlaceInput/PlaceInput";
+import { StyleSheet, View, Text } from 'react-native';
 
-const Component1 = () => {
-  const nextPage = useStore((state) => state.nextPage);
-  return (
-    <CreateProfileContent
-        title={"Привет, давай знакомиться"}
-        description={"Будь собой при выборе имени, привлекает больше внимания"}
-        Input={<NameInput />}
-        ContinueButton={<CountinueButton onNext={nextPage}/>}
-    />
-  );
-};
-
-const Component2 = () => {
-  const nextPage = useStore((state) => state.nextPage);
-  return (
-    <CreateProfileContent
-        title={"Где живем, там и ищем"}
-        description={"Выбери свой город, чтобы найти ближайщих соулмейтов"}
-        Input={<PlaceInput />}
-        ContinueButton={<CountinueButton onNext={nextPage} />}
-    />
-  );
-};
-
-const Component3 = () => {
-  const nextPage = useStore((state) => state.nextPage);
-  return (
-    <CreateProfileContent
-        title={"Выбери дату рождения"}
-        description={"Используй настояющую, ее потом поменять нельзя"}
-        Input={<DateInput />}
-        ContinueButton={<CountinueButton onNext={nextPage} />}
-    />
-  );
-};
-
-const Component4 = () => {
-  const nextPage = useStore((state) => state.nextPage);
-  return (
-    <CreateProfileContent
-        title={"Стань солцем среди планет"}
-        description={"Сделай свой слоган, который выделит тебя среди всех"}
-        Input={<AboutTextInput />}
-        ContinueButton={<CountinueButton onNext={nextPage} />}
-    />
-  );
-};
-
-const Component5 = () => {
-  return (
-    <CreateProfileContent
-        title={"Последний этап регистрации"}
-        description={"Выберите пол"}
-        Input={<ButtonChoice />}
-    />
-  );
-};
-
-const pages = [
-    Component1,
-    Component2,
-    Component3,
-    Component4,
-    Component5
+const data = [
+    {
+        title: 'Привет, давай знакомиться',
+        description: 'Будь собой при выборе имени, привлекает больше внимания.',
+        input: <NameInput />,
+        countinueButton: true,
+    },
+    {
+        title: 'Где живем, там и ищем',
+        description: 'Выбери свой город, чтобы найти ближайщих соулмейтов',
+        input: <PlaceInput />,
+        countinueButton: true,
+    },
+    {
+        title: 'Выбери дату рождения',
+        description: 'Используй настояющую, ее потом поменять нельзя',
+        input: <DateInput />,
+        countinueButton: true,
+    },
+    {
+        title: 'Стань солцем среди планет',
+        description: 'Сделай свой \"слоган\", который выделит тебя среди всех',
+        input: <AboutTextInput />,
+        countinueButton: true,
+    },
+    {
+        title: 'Последний этап регистрации',
+        description: 'Выберите пол',
+        input: <ButtonChoice />,
+        countinueButton: false,
+    },
 ];
 
-export default pages;
+const contentPages = data.map((item) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{item.title}</Text>
+        </View>
+    );
+});
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        
+    },
+});
+
+export default contentPages;
