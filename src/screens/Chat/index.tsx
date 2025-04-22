80e8b44b59f72cfef881a5ea7c46a0014cf6f815
AndDorosh <andrey.dorosh@list.ru>
2025-04-22 20:59:52 +0300

    feat: инициализация экрана чата

---
+++ src/screens/Chat/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import styles from './style'
+
+const Chat = () => {
+  return (
+    <View>
+      <Text>Chat</Text>
+    </View>
+  )
+}
+
+export default Chat
