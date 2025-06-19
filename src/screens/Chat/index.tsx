2eedf7c260d8f326c679de6d499bd2bb22ddcd48
Qfimf <visersampa@gmail.com>
2025-06-19 13:03:16 +0300

    -убраны Mock's

--- src/screens/Chat/index.tsx
+++ src/screens/Chat/index.tsx
@@ -1,64 +1,15 @@
 import Header from '@/src/components/chat/Header';
 import { FC } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from './style';
-import { closeModal, showModal } from '@/src/shared/hooks/useModal/index';
-import { Trash } from 'lucide-react-native';
-
 
 const Chat: FC = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Header />
             <Text>Chat</Text>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Button
-                title="Открыть модальное окно (закрытие по оверлею)"
-                onPress={() =>
-                    showModal({
-                        icon: <Trash />,
-                        title: 'Удалить аккаунт?',
-                        subTitle: 'Вы уверены, что хотите удалить аккаунт без права восстановления?',
-                        closeOnOverlayClick: true,
-                        buttons: [
-                            {
-                              title: 'Удалить',
-                              onPress: () => {},
-                              containerStyle: {
-                                backgroundColor: 'white',
-                                borderColor: 'red',
-                                borderWidth: 1,
-                              },
-                              textStyle: {
-                                color: 'red',
-                              },
-                            },
-                            {
-                              title: 'Отменить',
-                              onPress: closeModal,
-                              containerStyle: {
-                                backgroundColor: '#333333',
-                              },
-                              textStyle: {
-                                color: 'white',
-                              },
-                            },
-                          ]
-                          
-                    })}
-                
-            />
-            <Button
-                title="Открыть модальное окно (без закрытия по оверлею)"
-                onPress={() =>
-                    showModal({ title: 'Только по кнопке "назад"', closeOnOverlayClick: false },)
-                }
-            />
-            
-        </View>
         </SafeAreaView>
-        
     );
 };
 
